c86285c0c670ab9e5ae5f63f8e11c05eba64b7be
Rondrax2003 <rondrax2003@gmail.com>
2024-08-18 14:46:25 -0600

    se anadio un main.cpp y un makefile, ademas de hacersen algunas modificaciones y adiciones al hederfile y el .cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Node.h"
+using namespace std;
+
+int main(){
+    List Phone;
+    Phone.InsertOrdened(45);
+    Phone.InsertOrdened(50);
+    Phone.InsertOrdened(27);
+    Phone.InsertOrdened(100);
+    Phone.getsize();
+    Phone.~List();
+
+    return 0;
+}
